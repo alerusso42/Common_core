e8d5fc9584903a46d22af5df535b6455e09c4d95
Alessandro Russo <alerusso@c1r5p2.42firenze.it>
2025-04-16 15:59:58 +0200

    Ci siamo.

--- Minishell-test/executor/environment.c
+++ Minishell-test/executor/environment.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   environment.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerusso <alessandro.russo.frc@gmail.co    +#+  +:+       +#+        */
+/*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 10:26:15 by alerusso          #+#    #+#             */
-/*   Updated: 2025/04/14 10:52:55 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/04/16 15:51:40 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
+
+//NOTE - Returns the content of an item in the environment, if exist. 
+//				give **environment (NOT ADDRESS);
+//				the string to search; 
+//				an optional int ptr. If not NULL, it will be given the
+//				index of the env element, or -1 if not found.
+//				You can safely pass it to NULL.
+//NOTE - YOU MUST NOT FREE THE RETURNED STRING! It's env memory!!!
+
+char	*get_env(char **env, char *search)
+{
+	char	*item;
+
+	item = ft_getenv(env, search, NULL);
+	if (!item)
+		return (NULL);
+	while (*item != '=')
+		++item;
+	++item;
+	return (item);
+}
 
 //NOTE -	TO USE THESE FUNCTION, YOU MUST USE cpy_env FIRST!!!
 //			We need to copy the environment to heap first, and saving the
@@ -59,43 +80,6 @@ int	cpy_env(char **old_env, char ***new_env, int *env_size, int *last_env)
 	return (0);
 }
 
-//NOTE - Returns the content of an item in the environment, if exist. 
-//				give **environment (NOT ADDRESS);
-//				the string to search; 
-//				an optional int ptr. If not NULL, it will be given the
-//				index of the env element, or -1 if not found.
-//				You can safely pass it to NULL.
-//NOTE - YOU MUST NOT FREE THE RETURNED STRING! It's env memory!!!
-
-/*
-//REVIEW - ft_getenv
-//
-//		Operations:
-		1)	Finds the search string in the environment matrix, skipping '=';
-		2)	Where, if not NULL, is set to i if env[i], otherwise to -1;
-		3)	Returns env[i], that MUST NOT BE FREED.
-*/
-char	*ft_getenv(char **env, char *search, int *where)
-{
-	int		search_len;
-	int		item_len;
-	int		i;
-
-	search_len = ft_strlen(search);
-	i = 0;
-	while (env[i])
-	{
-		item_len = _sub_strlen(env[i], "=", EXCL);
-		if (!ft_strncmp(search, env[i], search_len) && \
-		!ft_strncmp(search, env[i], item_len))
-			break ;
-		++i;
-	}
-	if (where)
-		*where = (i * (env[i] != NULL)) + (-1 * (env[i] == NULL));
-	return (env[i]);
-}
-
 //NOTE - 	Returns this stuff: "~/42projects/Minishell$"
 //			Usage: give **environment (NOT ADDRESS), returns pwd location.
 //NOTE - YOU MUST FREE THE RETURNED STRING!	If NULL is returned, is E_MALLOC!
@@ -135,71 +119,4 @@ char	*get_pwd_address(char **env)
 		return (_ft_strjoin_free(ft_strdup("~/"), ft_strdup(pwd + home_len)));
 	}
 	return (NULL);
-}
-
-/*
-//REVIEW - expand_env
-
-//		Doubles the size of the environment.
-		Save the new size in the env_size variable.
-*/
-int	expand_env(char ***env, int *env_size, t_exec *exec)
-{
-	char	**new_env;
-	int		temp;
-
-	if (cpy_env(*env, &new_env, env_size, &temp) == E_MALLOC)
-		error(E_MALLOC, exec);
-	*env_size *= 2;
-	_free_matrix(*env);
-	*env = new_env;
-	return (0);
-}
-
-/*
-//REVIEW - env_pars
-
-//		The item to parse is divided in: 	PWD+=/execute
-											PWD  	+=		/execute
-											name	signs	content
-
-//		Operations:
-		1)	If the first element of the item is not a letter or a '_', 
-			return error;
-		2)	Skip characters until item[i] exists, 
-			is a number, a letter or '_';
-		3)	If the item[i] exists and is different than "=+",
-			return error;
-		4)	If you don't need to get other data, 
-			return 0;
-		5)	Set the size of the name to i;
-		6)	Set the size of the signs to 1 if item[i] is a '=' or '+',
-			otherwise set it to 0;
-		7)	If item[i] is a '+' and item[i + 1] is a '=', increase signs
-			to 1;
-		8)	We go to the end of the item;
-		9)	We set the size of cont_size to the difference of the total len
-			of the item - no_eq_plus signs - name_size.
-*/
-int	env_pars(char *item, int *no_eq_plus, int *name_size, int *cont_size)
-{
-	int	i;
-
-	if (!ft_isalpha(*item) && *item != '_')
-		return (E_ENV_PARSING);
-	i = 1;
-	while (item[i] && (ft_isalnum(item[i]) || item[i] == '_'))
-		++i;
-	if (item[i] && item[i] != '=' && item[i] != '+')
-		return (E_ENV_PARSING);
-	else if (no_eq_plus == NULL)
-		return (0);
-	*name_size = i;
-	*no_eq_plus = item[i] == '=' || item[i] == '+';
-	if (item[i] == '+' && item[i + 1] == '=')
-		*no_eq_plus += 1;
-	while (item[i])
-		++i;
-	*cont_size = i - *no_eq_plus - *name_size;
-	return (0);
 }
