8d36da18abfbf0a2fa12bfe463acf8d95ecbca4b
alerusso <alerusso@DESKTOP-9FJQO5K.localdomain>
2025-06-21 13:53:16 +0200

    Frame rate, test per mlx

---
+++ Archivio/mlx_test/h.h
@@ -0,0 +1,44 @@
+#ifndef H_H
+# define H_H
+
+# include "minilibx-linux/mlx.h"
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <math.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <sys/types.h>
+
+# define WIN_X 800
+# define WIN_Y 800
+# define TITLE "TEST"
+# define FPS 1
+# define FRAME_RATE ((1000 / FPS) * 1000)
+
+enum e_sprites
+{
+	EXAMPLE,
+	PICS_NUM,
+};
+
+typedef struct timeval	t_time;
+
+typedef struct s_mlx
+{
+	t_time		start;
+	void		*con;
+	void		*window;
+	void		*sprite[PICS_NUM];
+	int			start_x;
+	int			start_y;
+	int			variable_1_sprite_size;
+}	t_mlx;
+
+
+
+#endif
---
+++ Archivio/mlx_test/mouse/main.c
@@ -0,0 +1,152 @@
+#include "../h.h"
+
+//NOTE - Mouse coordinates
+
+/* int	main()
+{
+	t_mlx	mlx;
+	int		pos[2];
+
+	mlx = (t_mlx){0};
+	mlx.con = mlx_init();
+	mlx.window = mlx_new_window(mlx.con, WIN_X, WIN_Y, TITLE);
+	if (!mlx.window)
+		return (free(mlx.con), 1);
+	sleep(2);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	free(mlx.window);
+	free(mlx.con);
+} */
+
+//NOTE - Mouse timid
+//NOTE - Doesn't hide pointer: hides data
+
+/* int	main()
+{
+	t_mlx	mlx;
+	int		pos[2];
+
+	mlx = (t_mlx){0};
+	mlx.con = mlx_init();
+	mlx.window = mlx_new_window(mlx.con, WIN_X, WIN_Y, TITLE);
+	if (!mlx.window)
+		return (free(mlx.con), 1);
+	mlx_mouse_hide(mlx.con, mlx.window);
+	if (mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]) != 0)
+		printf("D..Don't look at me...\n");
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	sleep(2);
+	printf("Baka!\n");
+	mlx_mouse_show(mlx.con, mlx.window);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	sleep(1);
+	mlx_destroy_window(mlx.con, mlx.window);
+	mlx_destroy_display(mlx.con);
+	free(mlx.con);
+} */
+
+//NOTE - Mouse timid
+//NOTE - Moves mouse data
+
+/* int	main()
+{
+	t_mlx	mlx;
+	int		pos[2];
+
+	mlx = (t_mlx){0};
+	mlx.con = mlx_init();
+	mlx.window = mlx_new_window(mlx.con, WIN_X, WIN_Y, TITLE);
+	if (!mlx.window)
+		return (free(mlx.con), 1);
+	sleep(2);
+	mlx_mouse_move(mlx.con, mlx.window, 0, 0);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	sleep(1);
+	mlx_mouse_move(mlx.con, mlx.window, 110, 220);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	sleep(1);
+	mlx_mouse_move(mlx.con, mlx.window, 330, 220);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	sleep(1);
+	mlx_destroy_window(mlx.con, mlx.window);
+	mlx_destroy_display(mlx.con);
+	free(mlx.con);
+} */
+
+//NOTE - Frame rate
+
+/* static int	mouse(int keycode, t_mlx *mlx);
+static int	keyboard(int keycode, t_mlx *mlx);
+static int	loop(t_mlx *mlx);
+static long	elapsed_time(t_time start);
+
+int	main()
+{
+	t_mlx	mlx;
+	int		pos[2];
+
+	mlx = (t_mlx){0};
+	mlx.con = mlx_init();
+	mlx.window = mlx_new_window(mlx.con, WIN_X, WIN_Y, TITLE);
+	if (!mlx.window)
+		return (free(mlx.con), 1);
+	mlx_mouse_hook(mlx.window, mouse, &mlx);
+	mlx_key_hook(mlx.window, keyboard, &mlx);
+	mlx_mouse_get_pos(mlx.con, mlx.window, &pos[0], &pos[1]);
+	printf("Mouse is:\t%d\t%d\n", pos[0], pos[1]);
+	mlx_loop_hook(mlx.con, loop, &mlx);
+	gettimeofday(&mlx.start, NULL);
+	mlx_loop(mlx.con);
+	free(mlx.window);
+	free(mlx.con);
+}
+
+static int	keyboard(int keycode, t_mlx *mlx)
+{
+	printf("%d\n", keycode);
+	if (keycode == XK_Escape)
+	{
+		free(mlx->window);
+		free(mlx->con);
+		exit(0);
+	}
+	else if (keycode == XK_F1)
+		mlx_do_key_autorepeatoff(mlx->con);
+	else if (keycode == XK_F2)
+		mlx_do_key_autorepeaton(mlx->con);
+	return (0);
+}
+
+static int	mouse(int keycode, t_mlx *mlx)
+{
+	(void)keycode, (void)mlx;
+	return (0);
+}
+
+static int	loop(t_mlx *mlx)
+{
+
+	(void)mlx;
+	if (elapsed_time(mlx->start) > FRAME_RATE)
+	{
+		printf("Ciao\n");
+		gettimeofday(&mlx->start, NULL);
+	}
+	return (0);
+}
+
+static long	elapsed_time(t_time start)
+{
+	struct timeval	tv;
+	long			diff;
+
+	gettimeofday(&tv, NULL);
+	diff = (tv.tv_sec - start.tv_sec) * 1000000
+		 + (tv.tv_usec - start.tv_usec);
+	return (diff);
+} */
