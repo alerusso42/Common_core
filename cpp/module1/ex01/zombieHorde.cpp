3fcd94a5e6cd30712328380d9a4b0693e3f414d4
Alessandro Russo <alerusso@c1r4p3.42firenze.it>
2025-09-17 12:12:25 +0200

    debug mod 1

--- cpp/module1/ex01/zombieHorde.cpp
+++ cpp/module1/ex01/zombieHorde.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerusso <alessandro.russo.frc@gmail.co    +#+  +:+       +#+        */
+/*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 14:52:59 by alerusso          #+#    #+#             */
-/*   Updated: 2025/09/15 20:47:15 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/09/17 12:07:50 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Zombie* zombieHorde(int N, std::string name)
 
 	if (N < 0)
 		return (NULL);
-	horde = new Zombie[N + 1];
+	horde = new Zombie[N + 1]();
 	if (!horde)
 		return (NULL);
 	i = 0;
