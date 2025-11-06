47606521689dc9d24c44648329d3fbed0999c7ea
Alessandro Russo <alerusso@c2r2p12.42firenze.it>
2025-11-06 16:23:50 +0100

    module 7 corretto

--- cpp/module7/ex02/Array.hpp
+++ cpp/module7/ex02/Array.hpp
@@ -19,22 +19,22 @@ class	 	Array
 {
 private:
 	T			*_data;
-	u_int32_t	_size;
+	int32_t	_size;
 	enum	e_Array
 	{
-		ARRAY_MAXSIZE = (u_int32_t)1e8,
+		ARRAY_MAXSIZE = (int32_t)1e8,
 	};
 public:
 //	canonic form:
 	Array<T>(void);
-	Array<T>(u_int32_t size);
+	Array<T>(int32_t size);
 	~Array();
 	Array(const Array &other);
 	Array &operator=(const Array &other);
 
-	u_int32_t	size(void) const;
+	int32_t	size(void) const;
 	const T		*get_data(void) const;
-	T			&operator[](u_int32_t index);
+	T			&operator[](int32_t index);
 	void		print(void) const;
 };
 
