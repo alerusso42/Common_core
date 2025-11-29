8c0558c414a06a3ed0c5c1356ec408aaa9fb5323
alerusso42 <alessandro.russo.frc@gmail.com>
2025-11-29 18:13:07 +0000

    nessun progresso su Inception

---
+++ inception/progress_tracker/mariadb_cmds.md
@@ -0,0 +1,9 @@
+quit/exit					#	exits
+
+SELECT user FROM mysql.user;#	only with root mode: show all users
+
+SHOW DATABASES ;            #	show all databases
+
+USE database_name;			#	open a database
+
+SELECT * FROM table_name;	#	show every content from a table. You must be in a database.
--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -8,94 +8,64 @@
 
 #https://developer.wordpress.org/cli/commands/core/
 
-#!/bin/bash
-
 set -e
 
-until mysql -h mariadb -u"$DB_USER" -p"$DB_PASS" -e "select 1" >/dev/null 2>&1; do
-    sleep 1
-done
+# 1. Aspetta MariaDB
+echo "Attendo MariaDB..."
+sleep 10
 
+# 2. Se WP non è installato, fai il bootstrap
 if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "Prima installazione di WordPress."
+
+    # Scarica wp-cli
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+
+    # Scarica WordPress
     wp core download --allow-root
-    wp config create ... --allow-root
-    wp core install ... --allow-root
+
+    # Crea wp-config
+    wp config create \
+        --dbname="$WP_DB" \
+        --dbuser="$WP_USER" \
+        --dbpass="$WP_DB_PWD" \
+        --dbhost="$WP_DB_HOST" \
+        --allow-root \
+        --skip-check
+
+
+    # Installa WordPress solo al primo avvio
+    wp core install \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PWD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --skip-email \
+        --allow-root
+
+        chmod 777 /var/www/html/wp-content
+         wp user create $login $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+    wp theme install twentyfifteen
+
+    wp theme activate twentyfifteen
+
+    wp theme update twentyfifteen
+else
+    echo "WordPress già installato, nessuna modifica."
 fi
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir -p /var/www/html
+# 3. Sistema permessi per Nginx + php-fpm
+chown -R www-data:www-data /var/www/html
 
-cd /var/www/html
+# 4. Porta php-fpm 8.4 in modalità TCP su 9000 (NON socket)
+sed -i "s|listen = .*|listen = 9000|" /etc/php/8.4/fpm/pool.d/www.conf
 
-# remove all the wordpress files if there is something from the volumes to install it again
-rm -rf *
+# Assicura che gli errori vadano in foreground
+sed -i "s|^;daemonize = yes|daemonize = no|" /etc/php/8.4/fpm/php-fpm.conf
 
-# The commands are for installing and using the WP-CLI tool.
-
-# downloads the WP-CLI PHAR (PHP Archive) file from the GitHub repository. The -O flag tells curl to save the file with the same name as it has on the server.
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-echo FINO A CURL OK
-
-# makes the WP-CLI PHAR file executable.
-chmod +x wp-cli.phar 
-
-# moves the WP-CLI PHAR file to the /usr/local/bin directory, which is in the system's PATH, and renames it to wp. This allows you to run the wp command from any directory
-mv wp-cli.phar /usr/local/bin/wp
-
-# downloads the latest version of WordPress to the current directory. The --allow-root flag allows the command to be run as the root user, which is necessary if you are logged in as the root user or if you are using WP-CLI with a system-level installation of WordPress.
-wp core download --allow-root
-
-echo FINO A WP CORE DOWNLOAD OK
-
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-# change the those lines in wp-config.php file to connect with database
-
-#line 23
-sed -i -r "s/database/$WP_USER/1"   wp-config.php
-#line 26
-sed -i -r "s/database_user/$WP_USER/1"  wp-config.php
-#line 29
-sed -i -r "s/passwod/$WP_USER_PWD/1"    wp-config.php
-
-#line 32
-sed -i -r "s/localhost/mariadb/1"    wp-config.php
-
-echo FINO A SED OK
-
-echo $DOMAIN_NAME $WP_TITLE $WP_ADMIN $WP_ADMIN_PWD
-
-# installs WordPress and sets up the basic configuration for the site. The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password for the site's administrator account, and --admin_email sets the email address for the administrator. The --skip-email flag prevents WP-CLI from sending an email to the administrator with the login details.
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-echo wp install OK
-
-# creates a new user account with the specified username, email address, and password. The --role option sets the user's role to author, which gives the user the ability to publish and manage their own posts.
-wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-echo wp user create OK
-
-# installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
-wp theme install astra --activate --allow-root
-
-echo wp theme install OK
-
-echo FINO A installation ok
-
-wp plugin install redis-cache --activate --allow-root
-
-echo FINO A redis ok
-
-# uses the sed command to modify the www.conf file in the /etc/php/7.3/fpm/pool.d directory. The s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.3-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-# creates the /run/php directory, which is used by PHP-FPM to store Unix domain sockets.
-mkdir /run/php
-
-
-wp redis enable --allow-root
-
-
-# starts the PHP-FPM service in the foreground. The -F flag tells PHP-FPM to run in the foreground, rather than as a daemon in the background.
-/usr/sbin/php-fpm7.3 -F
+echo "Avvio php-fpm..."
+#exec /usr/sbin/php-fpm8.4 --nodaemonize
+/usr/sbin/php-fpm8.4 -F
