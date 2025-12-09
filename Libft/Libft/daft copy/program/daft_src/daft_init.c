a7c902f1a83240e8635643f82ca8ef394bb4e9f8
alerusso <alessandro.russo.frc@gmail.com>
2025-12-09 19:22:23 +0100

    before make

---
+++ Libft/Libft/daft copy/program/daft_src/daft_init.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   daft_init.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/15 22:36:58 by alerusso          #+#    #+#             */
+/*   Updated: 2025/11/27 19:00:37 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "daft_prog.h"
+
+static int	get_settings_file(t_daft_data *data);
+static int	get_database_file(t_daft_data *data);
+
+/*
+//REVIEW -	daft_get
+//
+	Initialize daft memory.
+	Returns 0 on success.
+	On failure, creates a log file named daft_log, 
+	writing on it the issue, and returns an integer
+	different than 0, based on a enum called e_daft_logs.
+*/
+int 	daft_init(void)
+{
+	t_daft_data	*data;
+
+	if (_daft_get_memory(NULL, false))
+		return (0);
+	data = ft_calloc(1, sizeof(t_daft_data));
+	if (!data)
+		return (DAFT_LOG_MALLOC);
+	data->conf.debug_log = true;
+	data->mem_size = -1;
+	data->minimal_matrix_num = 1;
+	_daft_get_memory(data, true);
+	if (get_settings_file(data) != 0)
+		return (daft_quit(), 1);
+	if (get_database_file(data) != 0)
+		return (daft_quit(), 1);
+	return (0);
+}
+
+//	DAFT_PWD is updated upon launching the update script.
+//	All setting file is read, searching for the two section of
+//	settings.
+static int	get_settings_file(t_daft_data *data)
+{
+	char	*line;
+	t_fd	settings_file;
+
+	settings_file = openfd(DAFT_PWD"/SETTINGS.md", "r");
+	if (!settings_file.n)
+		return (_daft_log(DAFT_LOG_SETT));
+	line = gnl();
+	while (line)
+	{
+		if (!ft_strncmp(line, "#SECTION - General settings", 27))
+		{
+			if (_daft_save_config(data) == DAFT_LOG_MALLOC)
+				closefd(settings_file);
+		}
+		else if (!ft_strncmp(line, "#SECTION - Files flags", 22))
+		{
+			if (_daft_save_fnames(data) == DAFT_LOG_MALLOC)
+				closefd(settings_file);
+		}
+		FREE(line);
+		line = gnl();
+	}
+	return (closefd(settings_file), 0);
+}
+
+//	For every file, a dedicated struct is allocated.
+//	The struct stores its inner flags, its filename, 
+static int	get_database_file(t_daft_data *data)
+{
+	int	i;
+
+	data->data_list = ft_calloc(data->files_num + 1, sizeof(t_daft_list *));
+	if (!data->data_list)
+		return (_daft_log(DAFT_LOG_MALLOC));
+	i = 0;
+	while (i != data->files_num)
+	{
+		data->data_list[i] = ft_calloc(1, sizeof(t_daft_list));
+		if (!data->data_list[i])
+			return (_daft_log(DAFT_LOG_MALLOC));
+		if (_daft_get_data(data->data_list[i], data->files_names[i], i) != 0)
+			return (data->last_error);
+		++i;
+	}
+	return (0);
+}
---
+++ Libft/Libft/daft copy/program/daft_src/daft_utils_append.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   daft_utils_append.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alerusso <alessandro.russo.frc@gmail.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 15:07:11 by alerusso          #+#    #+#             */
+/*   Updated: 2025/11/30 20:13:34 by alerusso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "daft_prog.h"
+
+static void	append(t_daft_data *dt, t_daft_mem *mem, int fn);
+
+void	_daft_append_data(t_daft_data *data, t_daft_list *file, int file_n)
+{
+	t_daft_mem	*mem;
+	t_daft_mem	*next;
+	int			i;
+
+	if (_daft_append_files(data, file->filename, file->filename))
+		return ;
+	i = 0;
+	mem = data->old_mem;
+	while (mem)
+	{
+		next = mem->next;
+		data->mem.add = i;
+		if (mem->offset)
+			append(data, mem, file_n);
+		else
+			i++;
+		mem = next;
+	}
+	closefd(data->temp_files[0]);
+	closefd(data->temp_files[1]);
+}
+
+static void	append(t_daft_data *dt, t_daft_mem *mem, int fn)
+{
+	char	*line;
+
+	SEEK(dt->temp_files[0].p, mem->offset, SEEK_SET);
+	line = gnl();
+	if (!line)
+	{
+		_daft_free_mem(dt, dt->mem.add);
+		return ;
+	}
+	fd_printf(dt->temp_files[1], "\n");
+	_daft_append_f(dt, fn, &line, mem);
+	FREE(line);
+}
+
